cf0c43086178c215337404f90c9ef619f9681c1a
ArturKraw <artur.krawczyk64@gmail.com>
2018-01-26 12:12:00 +0100

    Modul 14 - Zadanie 14.4 (wersja 0.4) (poprwiona)

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -15,24 +15,26 @@ public class TaskController {
        return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks", params = {"taskId"})
+    @RequestMapping(method = RequestMethod.GET, value = "getTask", params = {"taskId"})
     @ResponseBody
     public TaskDto getTask(@RequestParam("taskId") String taskId){
         return new TaskDto((long)1, "test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-        public void deleteTask(String taskId){
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask", params = {"taskId"})
+            public void deleteTask(@RequestParam("taskId") String taskId){
+        System.out.println("Delete Task(id=taskId): " + taskId);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask", params = {"taskDto"})
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     @ResponseBody
-    public TaskDto updateTask(@RequestParam("taskDto") TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask", params = {"taskDto"} )
+    @RequestMapping(method = RequestMethod.POST, value = "createTask" )
     @ResponseBody
-    public void createTask (@RequestParam("taskDto")TaskDto taskDto){
+    public TaskDto createTask (@RequestBody TaskDto taskDto){
+        return new TaskDto((long)1, "Ceated Test line", "New test content");
     }
 }
