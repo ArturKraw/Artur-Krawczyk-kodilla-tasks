87bd222c0b67ba8ff7ae12f158459a3e7c01fb12
ArturKraw <artur.krawczyk64@gmail.com>
2018-02-20 14:06:03 +0100

    Modul 17 - Zadanie 17.4 (wersja 1.0) (działa - do sprawdzenia)

--- showtasks.bat
+++ showtasks.bat
@@ -1,19 +1,14 @@
 call runcrud.bat
-if "%ERRORLEVEL%" == "0" goto open_browser
+if "%ERRORLEVEL%" == "0" goto browse_thispage
 echo.
 echo RUNCRUD has errors - breaking work
 goto fail
 
-:open_browser
-start chrome https://www.google.com
-if "%ERRORLEVEL%" == "0" goto open_thispage
-echo Cannot open browser
-goto fail
-
-:open_thispage
+:browse_thispage
 start chrome http://localhost:8080/crud/v1/task/getTasks
 if "%ERRORLEVEL%" == "0" goto end
-goto end
+echo Cannot open browser
+goto fail
 
 :fail
 echo.
