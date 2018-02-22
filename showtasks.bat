beb8d367dbd677a16ab40daaa82a4bcb7f0fb2bb
ArturKraw <artur.krawczyk64@gmail.com>
2018-02-22 11:49:27 +0100

    Modul 17 - Zadanie 17.4 (wersja 1.1)
    (zapisan pod nazwa zadanie 17.5)

--- showtasks.bat
+++ showtasks.bat
@@ -5,9 +5,9 @@ echo RUNCRUD has errors - breaking work
 goto fail
 
 :browse_thispage
-start chrome http://localhost:8080/crud/v1/task/getTasks
+@start firefox http://localhost:8080/crud/v1/task/getTasks
 if "%ERRORLEVEL%" == "0" goto end
-echo Cannot open browser
+echo Cannot open browser or/and this page
 goto fail
 
 :fail
