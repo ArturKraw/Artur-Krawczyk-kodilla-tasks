555ef144332cb5dcdada59a58509409a61e4dd8c
ArturKraw <artur.krawczyk64@gmail.com>
2018-02-27 23:32:37 +0100

    Modul 18 - Zadanie 18.2 (wersja 0.3) tresc przykladu - działa

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -19,37 +19,9 @@ public class TrelloBoardDto {
     private String name;
     private String id;
 
-    @Value("${trello.api.endpoint.prod)")
-    private String trelloApiEndpoint;
-
-    @Value("${trello.app.key)")
-    private String trelloAppKey;
-
-    @Value("${trello.app.token)")
-    private String trelloToken;
 
 
-    @Autowired
-    private RestTemplate restTemplate;
 
-    public List<TrelloBoardDto> getTrelloBoards () {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/arturkrawczyk7/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id")
-                .build().encode().toUri();
-
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
-                //"https://api.trello.com/1/memebers/arturkrawczyk7/boards", TrelloBoardDto[].class);
-                //trelloApiEndpoint + "/members/arturkrawczyk7/boards" + "?key=" + trelloAppKey + "?token=" +trelloToken,
-                //TrelloBoardDto[].class);
-
-        if (boardsResponse != null) {
-            return Arrays.asList(boardsResponse);
-        }
-        return new ArrayList<>();
-
-    }
 
 }
