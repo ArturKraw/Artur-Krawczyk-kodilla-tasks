119d5cc6ba924d55633b6e2247d7c53c63b84eeb
ArturKraw <artur.krawczyk64@gmail.com>
2018-02-28 13:16:23 +0100

    Modul 18 - Zadanie 18.2 (wersja 0.3) działający przyklad

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -21,7 +21,8 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId()
+                + " " + trelloBoardDto.getName()));
 
     }
 }
--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -15,13 +15,13 @@ import java.util.List;
 @Component
 public class TrelloClient {
 
-    @Value("${trello.api.endpoint.prod)")
+    @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
 
-    @Value("${trello.app.key)")
+    @Value("${trello.app.key}")
     private String trelloAppKey;
 
-    @Value("${trello.app.token)")
+    @Value("${trello.app.token}")
     private String trelloToken;
 
 
@@ -30,7 +30,8 @@ public class TrelloClient {
 
     public List<TrelloBoardDto> getTrelloBoards () {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/arturkrawczyk7/boards")
+        URI url = UriComponentsBuilder
+                .fromHttpUrl(trelloApiEndpoint + "/members/arturkrawczyk7/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id")
